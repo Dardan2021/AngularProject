1a578b780121b3516b374d0d73b7c7bc89723f64
Dardan2021 <78737429+Dardan2021@users.noreply.github.com>
2021-11-27 13:05:19 +0100

    dashboard Creation

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,8 +8,28 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
 
   constructor() { }
+  Designation: string | undefined;
+  Username: string | undefined;
+  NoOfTeamMembers: number | undefined;
+  TotalCastOfAllProject: number | undefined
+  PendingTasks: number | undefined
+  UpcomingProject: number | undefined
+  ProjectCost : number | undefined
+  CurrentExpenditures : number | undefined
+  AvailableFunds : number | undefined
 
   ngOnInit(): void {
+    this.Designation="Team Leader";
+    this.Username="Dardan";
+    this.NoOfTeamMembers= 23;
+    this.TotalCastOfAllProject=34;
+    this.PendingTasks=44;
+    this.UpcomingProject=44;
+    this.ProjectCost=3234;
+    this.CurrentExpenditures=2342;
+    this.AvailableFunds=234234;
+
+
   }
 
 }
